8708cdc3f4b47227f1e44714e81d9a6d9790fddc
philtipping31 <>
2023-12-21 12:22:26 +0000

    added functions and variables to display question and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,190 @@
-/* quiz content */
+/* lyrics and asnwers content */
 
 
-const quizSection = [{
-        lyrics: "Somebody once told me the world was gonna roll me, I ain't the ________ tool in the shed.",
-        a: "Best",
-        b: "Sharpest",
-        c: "Rusty",
-        d: "Oldest",
-        correct: "b",
-    },
+const questions = [
     {
-        lyrics: "He was a boy, she was a girl, can I make it any more obvious? He was a ________, she did ballet what more can I say?",
-        a: "Square",
-        b: "Hunk",
-        c: "Coder",
-        d: "Punk",
-        correct: "d",
+        question: "Somebody once told me the world was gonna roll me, I ain't the ________ tool in the shed.",
+        answers: [
+            { text: "Best", correct: false },
+            { text: "Sharpest", correct: true },
+            { text: "Rusty", correct: false },
+            { text: "Oldest", correct: false },
+        ]
     },
-    {
-        lyrics: "Cos i'm just a ________, dirtbag baby like you",
-        a: "Smelly",
-        b: "Gorgeous",
-        c: "Teenage",
-        d: "Retired",
-        correct: "c",
-    },
-    {
-        lyrics: "Today I don't feel like doing ________ ",
-        a: "Anything",
-        b: "Work",
-        c: "The housework",
-        d: "Maths",
-        correct: "a",
-    },
-    {
-        lyrics: "Since you been gone, I can ________ for the first time",
-        a: "Sleep",
-        b: "Run",
-        c: "Breathe",
-        d: "Crawl",
-        correct: "c",
-    },
-    {
-        lyrics: "You shoot me down, but I won't fall, I am ________",
-        a: "Invinsible",
-        b: "Titanium",
-        c: "The Terminator",
-        d: "Strong",
-        correct: "b",
-    },
-    {
-        lyrics: "Jealousy, turning saints into the sea, swimming through sick lullabies, choking on your ________",
-        a: "Alibis",
-        b: "Dinner",
-        c: "Words",
-        d: "Own",
-        correct: "a",
-    },
-    {
-        lyrics: "So I'll start a revolution from my bed, cause you said the ________ I had went to my head",
-        a: "Drink",
-        b: "Brains",
-        c: "Stars",
-        d: "None of these",
-        correct: "b",
-    },
-    {
-        lyrics: "I want a brand new house on an episode of Cribs and a bathroom I can ________ in ",
-        a: "sunabathe",
-        b: "eat lunch",
-        c: "bathe",
-        d: "play baseball",
-        correct: "d",
-    },
-    {
-        lyrics: "Today is gonna be the day that they're gonna ________ it back to you",
-        a: "give",
-        b: "fire",
-        c: "throw",
-        d: "drop",
-        correct: "c",
-    },
-    {
-        lyrics: "A long, long time ago, I can still remember how that music used to make me________  ",
-        a: "cry",
-        b: "smile",
-        c: "happy",
-        d: "excited",
-        correct: "b",
-    },
-    {
-        lyrics: "I'm not the man they think I am at home, Oh, no, no, no. I'm a ________ man",
-        a: "rocket",
-        b: "lonely",
-        c: "happy",
-        d: "scary",
-        correct: "a",
-    },
-    {
-        lyrics: "Sometimes I feel like ________ my hands up in the air ",
-        a: "waving",
-        b: "chucking",
-        c: "putting",
-        d: "throwing",
-        correct: "d",
-    },
-    {
-        lyrics: "We get it almost every night, When the ________ is big and bright",
-        a: "moon",
-        b: "sta",
-        c: "",
-        d: "",
-        correct: "",
-    },
-    {
-        lyrics: "________  ",
-        a: "",
-        b: "e",
-        c: "",
-        d: "",
-        correct: "",
-    },
-    
 
+    {
+        question: "He was a boy, she was a girl, can I make it any more obvious? He was a ________, she did ballet what more can I say?",
+        answers: [
+            { text: "Square", correct: false },
+            { text: "Hunk", correct: false },
+            { text: "Coder", correct: false },
+            { text: "Punk", correct: true },
+        ]
+    },
 
+    {
+        question: "Cos i'm just a ________, dirtbag baby like you",
+        answers: [
+            { text: "Smelly", correct: false },
+            { text: "Gorgeous", correct: false },
+            { text: "Teenage", correct: true },
+            { text: "Retired", correct: false },
+        ]
+    },
 
-    
-    
+    {
+        question: "Today I don't feel like doing ________ ",
+        answers: [
+            { text: "Anything", correct: true },
+            { text: "Work", correct: false },
+            { text: "The housework", correct: false },
+            { text: "Maths", correct: false },
+        ]
+    },
+
+    {
+        question: "Since you been gone, I can ________ for the first time",
+        answers: [
+            { text: "Sleep", correct: false },
+            { text: "Run", correct: false },
+            { text: "Breathe", correct: true },
+            { text: "Crawl", correct: false },
+        ]
+    },
+
+    {
+        question: "You shoot me down, but I won't fall, I am ________",
+        answers: [
+            { text: "Invinsible", correct: false },
+            { text: "Titanium", correct: true },
+            { text: "The Terminator", correct: false },
+            { text: "Strong", correct: false },
+        ]
+    },
+
+    {
+        question: "Jealousy, turning saints into the sea, swimming through sick lullabies, choking on your ________",
+        answers: [
+            { text: "Alibis", correct: true },
+            { text: "Dinner", correct: false },
+            { text: "Words", correct: false },
+            { text: "Own", correct: false },
+        ]
+    },
+
+    {
+        question: "So I'll start a revolution from my bed, cause you said the ________ I had went to my head",
+        answers: [
+            { text: "Drink", correct: false },
+            { text: "Brains", correct: true },
+            { text: "Stars", correct: false },
+            { text: "None of these", correct: false },
+        ]
+    },
+
+    {
+        question: "I want a brand new house on an episode of Cribs and a bathroom I can ________ in ",
+        answers: [
+            { text: "sunabathe", correct: false },
+            { text: "eat lunch", correct: false },
+            { text: "play football", correct: false },
+            { text: "play baseball", correct: true },
+        ]
+    },
+
+    {
+        question: "Today is gonna be the day that they're gonna ________ it back to you",
+        answers: [
+            { text: "give", correct: false },
+            { text: "fire", correct: false },
+            { text: "throw", correct: true },
+            { text: "drop", correct: false },
+        ]
+    },
+
+    {
+        question: "A long, long time ago, I can still remember how that music used to make me________  ",
+        answers: [
+            { text: "cry", correct: false },
+            { text: "smile", correct: true },
+            { text: "happy", correct: false },
+            { text: "excited", correct: false },
+        ]
+    },
+
+    {
+        question: "I'm not the man they think I am at home, Oh, no, no, no. I'm a ________ man",
+        answers: [
+            { text: "rocket", correct: true },
+            { text: "lonely", correct: false },
+            { text: "happy", correct: false },
+            { text: "scary", correct: false },
+        ]
+    },
+
+    {
+        question: "Sometimes I feel like ________ my hands up in the air ",
+        answers: [
+            { text: "scary", correct: false },
+            { text: "chucking", correct: false },
+            { text: "putting", correct: false },
+            { text: "throwing", correct: true },
+        ]
+    },
+
+    {
+        question: "We get it almost every night, When the ________ is big and bright",
+        answers: [
+            { text: "moon", correct: false },
+            { text: "stars", correct: false },
+            { text: "sun", correct: false },
+            { text: "lampshade", correct: false },
+        ]
+    },
+
 ];
+
+/* declaration of variables */
+
+const questionElement = document.getElementById("lyrics");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+
+/* Quiz Functions */
+
+function startQuiz() {
+    let currentQuestionIndex = 0;
+    let score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+
+startQuiz();
+
+
